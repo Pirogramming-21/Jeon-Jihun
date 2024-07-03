04500cd8f46e4ab62f863b6c9d8acee6dae9dab7
Jihun Jeon <jihunee01@gmail.com>
2024-07-03 15:39:36 +0900

    Javascript - fetch

---
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -0,0 +1,16 @@
+
+// Fetch the items from the JSON file
+function loadItems() {
+    return fetch('data/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
+}
+
+
+// main
+loadItems()
+    .then(items => {
+    //    displayItems(items);
+    //    setEventLisners(items)
+    })
+    .catch(console.log);
