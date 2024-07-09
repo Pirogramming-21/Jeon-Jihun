f8c94ea9699bd65e0d9515e1a253f777ec19cd8d
Jihun Jeon <jihunee01@gmail.com>
2024-07-10 01:11:23 +0900

    나의 첫 번째 Django 프로젝트!

--- djangogirls/python_intro.py
+++ djangogirls/python_intro.py
@@ -1,7 +1,7 @@
 # 문자열
-print("Hi there"+"01a")
+print("Hi there" + "01a")
 print("01a" * 3)
-print('Runnin\' down the hill')
+print("Runnin' down the hill")
 print("ola".upper())
 
 # 오류
@@ -28,46 +28,46 @@ lottery.pop(0)
 print(lottery)
 
 # 딕셔너리
-participant = {'name' : "Ola", "country": "Poland",'favorite_numbers': [7, 42, 92]}
+participant = {"name": "Ola", "country": "Poland", "favorite_numbers": [7, 42, 92]}
 print(participant["name"])
-participant["favorite_language"] = 'Python'
-participant.pop('favorite_numbers')
-participant['country'] = 'Germany'
+participant["favorite_language"] = "Python"
+participant.pop("favorite_numbers")
+participant["country"] = "Germany"
 print(participant)
 
 # 비교
-print(5>2)
-print(3<1)
-print(5 > 2*2)
+print(5 > 2)
+print(3 < 1)
+print(5 > 2 * 2)
 print(1 == 1)
 print(5 != 2)
 
-print(6>2 and 2<3)
-print(3>2 and 2<1)
-print(3>2 or 2<1)
+print(6 > 2 and 2 < 3)
+print(3 > 2 and 2 < 1)
+print(3 > 2 or 2 < 1)
 
 # 불리언
 print(True and True)
 print(False and True)
-print(True or 1==1)
-print(1 !=2)
+print(True or 1 == 1)
+print(1 != 2)
 
-#if else
-if 3>2:
+# if else
+if 3 > 2:
     print("It works!")
 
-if 5<2:
-    print('5 is indeed greater than 2')
+if 5 < 2:
+    print("5 is indeed greater than 2")
 else:
-    print('5 is not greater than 2')
+    print("5 is not greater than 2")
 
-name = 'Sonja'
-if name == 'Ola':
-    print('Hey Ola!')
-elif name == 'Sonja':
-    print('Hey Sonja!')
+name = "Sonja"
+if name == "Ola":
+    print("Hey Ola!")
+elif name == "Sonja":
+    print("Hey Sonja!")
 else:
-    print('Hey anonymous!')
+    print("Hey anonymous!")
 
 volume = 57
 if volume < 20:
@@ -83,38 +83,46 @@ elif 80 <= volume < 100:
 else:
     print("My ears are hurting! :(")
 
+
 # 나만의 함수 만들기
 def hi():
-    print('Hi there!')
-    print('How are you?')
+    print("Hi there!")
+    print("How are you?")
+
 
 hi()
 
+
 def hi(name):
-    if name == 'Ola':
-        print('Hi Ola!')
-    elif name == 'Sonja':
-        print('Hi Sonja!')
+    if name == "Ola":
+        print("Hi Ola!")
+    elif name == "Sonja":
+        print("Hi Sonja!")
     else:
-        print('Hi anonymous!')
+        print("Hi anonymous!")
+
 
 hi("Ola")
 hi("Sonja")
 
+
 def hi(name):
-    print('Hi ' + name + '!')
+    print("Hi " + name + "!")
+
 
 hi("Rachel")
 
 # 반복하기
 
-def hi(name):
-    print('Hi ' + name + '!')
 
-girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+def hi(name):
+    print("Hi " + name + "!")
+
+
+girls = ["Rachel", "Monica", "Phoebe", "Ola", "You"]
 for name in girls:
     hi(name)
-    print('Next girl')
+    print("Next girl")
 
 for i in range(1, 6):
     print(i)
